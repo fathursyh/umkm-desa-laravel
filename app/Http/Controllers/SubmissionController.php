7b02aa78b27c5c23dfc85639a24e4ab37af592ea
naufal-alwi-p <naufalpratama992@gmail.com>
2025-02-20 05:53:14 +0700

    Memperbaiki validasi submission dan membuat hanya user umkm saja yang bisa mengirim submission

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -25,15 +25,16 @@ class SubmissionController extends Controller
 
     public function store(Request $request)
     {
-        $tahun = $request->input('establishment_date');
-        dd($tahun);
+        // Check if user is not admin
+        if (Auth::user()->role === 'admin') {
+            abort(403);
+        }
+
         $request->validate([
-            'umkm_name' => 'required|string',
-            'establishment_date' => 'required|date',
-            'business_type' => 'required|string',
-            'description' => 'required|string',
-            'application_letter' => 'required|mimes:pdf|max:5120',
-            'rab_document' => 'required|mimes:pdf|max:5120',
+            'tujuan_pengajuan' => 'required|string|min:1|max:255',
+            'pendapatan_bulan' => 'required|numeric|decimal:0,2|min:0|max:9999999999999.99',
+            'application_letter' => 'required|file|mimes:pdf|max:5120',
+            'rab_document' => 'required|file|mimes:pdf|max:5120',
         ]);
 
         $submission = new Submission($request->except(['application_letter', 'rab_document']));
