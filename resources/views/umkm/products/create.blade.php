d23451ecb2445206446e9d52e2f035fbfa76a8ae
naufal-alwi-p <naufalpratama992@gmail.com>
2025-02-20 20:08:56 +0700

    Memperbaiki fitur CRUD Products

--- resources/views/umkm/products/create.blade.php
+++ resources/views/umkm/products/create.blade.php
@@ -2,23 +2,43 @@
 
 @section('content')
 <div class="container">
+    @session('success')
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ $value }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endsession
+
     <h1>Tambah Produk</h1>
     <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="name">Nama Produk</label>
-            <input type="text" class="form-control" id="name" name="name" required>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
+            @error('name')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="description">Deskripsi Produk</label>
-            <textarea class="form-control" id="description" name="description" rows="4" required></textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="4" required>{{ old('description') }}</textarea>
+            @error('description')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="images">Gambar Produk</label>
-            <input type="file" class="form-control" id="images" name="images[]" multiple required>
+            <input type="file" class="form-control @error('images.*') is-invalid @enderror" id="images" name="images[]" multiple required>
+            @error('images.*')
+                @foreach ($errors->get('images.*') as $errorMessages)
+                    @foreach ($errorMessages as $error)
+                        <p class="text-danger">{{ $error }}</p>
+                    @endforeach
+                @endforeach
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-success">Simpan</button>
